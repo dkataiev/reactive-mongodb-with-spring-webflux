870d0402e4495d57ec2e138e625194916c1ecf31
Dmytro Kataiev <d.kataiev@gmail.com>
2022-01-14 11:41:15 +0200

    Add data model

---
+++ src/main/java/lab/dkataiev/reactive/webflux/mongo/model/Project.java
@@ -0,0 +1,35 @@
+package lab.dkataiev.reactive.webflux.mongo.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Data
+@Document(collation = "projects")
+public class Project {
+
+    @Id
+    private String _id;
+
+    private String name;
+
+    @Field("desc")
+    private String description;
+
+    private String startDate;
+
+    private String endDate;
+
+    @Field("cost")
+    private long estimatedCost;
+
+    private List<String> countryList;
+
+    @Version
+    private long version;
+
+}
---
+++ src/main/java/lab/dkataiev/reactive/webflux/mongo/model/Task.java
@@ -0,0 +1,30 @@
+package lab.dkataiev.reactive.webflux.mongo.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@Document(collation = "tasks")
+public class Task {
+
+    @Id
+    private String _id;
+
+    @Field("pid")
+    private String projectId;
+
+    private String name;
+
+    @Field("desc")
+    private String description;
+
+    private String ownerName;
+
+    private long cost;
+
+    @Version
+    private long version;
+}
